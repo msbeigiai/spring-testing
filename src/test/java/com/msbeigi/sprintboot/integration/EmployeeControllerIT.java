3c405b6cbb1a73f6ce095b09bea02d033e1967d4
msbeigiai <msbeigi83@gmail.com>
2023-11-24 19:59:47 +0200

    setup project for integration test

---
+++ src/test/java/com/msbeigi/sprintboot/integration/EmployeeControllerIT.java
@@ -0,0 +1,28 @@
+package com.msbeigi.sprintboot.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.msbeigi.sprintboot.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class EmployeeControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        employeeRepository.deleteAll();
+    }
+}
