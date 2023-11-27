78622c779300d5659e62e7ef276aa00d10f80280
msbeigiai <msbeigi83@gmail.com>
2023-11-27 22:31:08 +0200

    Add create employee endpoint

--- springboot-webflux-test/src/main/java/com/msbeigi/webfluxtest/controller/EmployeeController.java
+++ springboot-webflux-test/src/main/java/com/msbeigi/webfluxtest/controller/EmployeeController.java
@@ -1,9 +1,23 @@
 package com.msbeigi.webfluxtest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.msbeigi.webfluxtest.dto.EmployeeDto;
+import com.msbeigi.webfluxtest.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Mono<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto) {
+        return employeeService.saveEmployee(employeeDto);
+    }
 }
