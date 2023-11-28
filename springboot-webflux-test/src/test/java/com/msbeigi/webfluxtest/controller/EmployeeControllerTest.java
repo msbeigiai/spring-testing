7f0672f13a9538e86dfd73d9258ee02d931367b9
msbeigiai <msbeigi83@gmail.com>
2023-11-28 21:28:14 +0200

    Test create employee reactive

---
+++ springboot-webflux-test/src/test/java/com/msbeigi/webfluxtest/controller/EmployeeControllerTest.java
@@ -0,0 +1,72 @@
+package com.msbeigi.webfluxtest.controller;
+
+import com.msbeigi.webfluxtest.dto.EmployeeDto;
+import com.msbeigi.webfluxtest.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(controllers = EmployeeController.class)
+class EmployeeControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Test
+    public void givenEmployeeObject_whenSaveEmployee_thenReturnSavedEmployee() {
+        // given - precondition or setup
+        EmployeeDto employeeDto = EmployeeDto
+                .builder()
+                .firstName("Mohsen")
+                .lastName("Sadeghbeigi")
+                .email("mohsen.sadegh62@gmail.com")
+                .build();
+
+        given(employeeService.saveEmployee(any(EmployeeDto.class)))
+                .willReturn(Mono.just(employeeDto));
+
+        // when - action and the behaviour that we are going to test
+        WebTestClient.ResponseSpec responseSpec = webTestClient
+                .post()
+                .uri("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(employeeDto), EmployeeDto.class)
+                .exchange();
+
+        // then - verify the output
+        responseSpec
+                .expectStatus()
+                .isCreated()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .jsonPath("$.firstName").isEqualTo(employeeDto.getFirstName())
+                .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
+                .jsonPath("$.email").isEqualTo(employeeDto.getEmail());
+    }
+}
+
+
+
+
+
+
+
+
+
