e26be4821459a59ee1d1989439d55d18610efaa5
msbeigiai <msbeigi83@gmail.com>
2023-11-28 21:35:23 +0200

    Implement getEmployeeById test

--- springboot-webflux-test/src/test/java/com/msbeigi/webfluxtest/controller/EmployeeControllerTest.java
+++ springboot-webflux-test/src/test/java/com/msbeigi/webfluxtest/controller/EmployeeControllerTest.java
@@ -14,6 +14,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
@@ -54,6 +56,37 @@ class EmployeeControllerTest {
         responseSpec
                 .expectStatus()
                 .isCreated()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .jsonPath("$.firstName").isEqualTo(employeeDto.getFirstName())
+                .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
+                .jsonPath("$.email").isEqualTo(employeeDto.getEmail());
+    }
+
+    @Test
+    public void givenEmployeeId_whenGetEmployeeById_thenReturnEmployeeObject() {
+        // given - precondition or setup
+        EmployeeDto employeeDto = EmployeeDto
+                .builder()
+                .firstName("Mohsen")
+                .lastName("Sadeghbeigi")
+                .email("mohsen.sadegh62@gmail.com")
+                .build();
+
+        String employeeId = "123";
+
+        given(employeeService.getEmployee(employeeId)).willReturn(Mono.just(employeeDto));
+
+        // when - action and the behaviour that we are going to test
+        WebTestClient.ResponseSpec responseSpec = webTestClient
+                .get()
+                .uri("/api/employees/{id}", Collections.singletonMap("id", employeeId))
+                .exchange();
+
+        // then - verify the output
+        responseSpec
+                .expectStatus()
+                .isOk()
                 .expectBody()
                 .consumeWith(System.out::println)
                 .jsonPath("$.firstName").isEqualTo(employeeDto.getFirstName())
