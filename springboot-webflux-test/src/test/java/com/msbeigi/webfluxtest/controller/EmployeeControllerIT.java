ed876e0aafcc07ba6062062051725990584c2ff5
msbeigiai <msbeigi83@gmail.com>
2023-11-29 22:33:29 +0200

    Implement getEmployeeById test

--- springboot-webflux-test/src/test/java/com/msbeigi/webfluxtest/controller/EmployeeControllerIT.java
+++ springboot-webflux-test/src/test/java/com/msbeigi/webfluxtest/controller/EmployeeControllerIT.java
@@ -2,6 +2,9 @@ package com.msbeigi.webfluxtest.controller;
 
 import com.msbeigi.webfluxtest.dto.EmployeeDto;
 import com.msbeigi.webfluxtest.service.EmployeeService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +18,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.Collections;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // will load application context
 public class EmployeeControllerIT {
@@ -24,7 +28,6 @@ public class EmployeeControllerIT {
 
     @Autowired
     private WebTestClient webTestClient;
-
 
     @Test
     public void givenEmployee_whenSaveEmployee_thenShouldReturnNewEmployeeSaved() {
@@ -49,6 +52,37 @@ public class EmployeeControllerIT {
                 .jsonPath("$.firstName").isEqualTo(employeeDto.getFirstName())
                 .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
                 .jsonPath("$.email").isEqualTo(employeeDto.getEmail());
+    }
+
+    @Test
+    public void givenEmployeeId_whenGetEmployeeById_thenReturnAnEmployeeObject() {
+        // given - precondition or setup
+        EmployeeDto employeeDto = EmployeeDto.builder()
+                .firstName("Sadegh")
+                .lastName("Mohammadi")
+                .email("sadegh.mohammadi@gmail.com")
+                .build();
+
+        EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto).block();
+
+        // when - action and the behaviour that we are going to test
+
+        Assertions.assertThat(savedEmployee).isNotNull();
+
+        webTestClient
+                .get()
+                .uri("/api/employees/{id}", Collections.singletonMap("id", savedEmployee.getId()))
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .jsonPath("$.id").isEqualTo(savedEmployee.getId())
+                .jsonPath("$.firstName").isEqualTo(savedEmployee.getFirstName())
+                .jsonPath("$.lastName").isEqualTo(savedEmployee.getLastName())
+                .jsonPath("$.email").isEqualTo(savedEmployee.getEmail());
+
+        // then - verify the output
     }
 
 }
